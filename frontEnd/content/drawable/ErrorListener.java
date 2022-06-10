d3387f351de83849622def7d1095ecd1a03f462d
Martin-Paez <104704565+Martin-Paez@users.noreply.github.com>
2022-06-09 23:21:31 -0300

    Add files via upload

---
+++ frontEnd/content/drawable/ErrorListener.java
@@ -0,0 +1,11 @@
+package frontEnd.content.drawable;
+
+public interface ErrorListener {
+
+	/**
+	 *  Utilizado para avisar de que se produjo un error 
+	 */
+	public void fatalError(String errorMsg);
+	
+	
+}
